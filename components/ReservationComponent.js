1134d5b78d2e10b40b78c5adff10cec48335814c
Dale Mabilangan <dalemabilangan@Dales-iMac.fios-router.home>
2021-01-20 18:46:17 -0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -37,7 +38,8 @@ class Reservation extends Component {
               { 
                 text: "OK",
                 onPress: () => {
-                  this.resetForm();
+                    this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                    this.resetForm();
                 },
               },
             ],
@@ -55,6 +57,31 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
 
     render() {
         return (
